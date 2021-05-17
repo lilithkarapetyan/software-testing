7c450288b8f8c0bccd469bde27f289049e568b89
lilithkarapetyan <lilith.karapetyann@gmail.com>
2021-05-17 20:26:21 +0400

    fix

--- hw3.3/webdriver_java/src/test/java/base/BaseTests.java
+++ hw3.3/webdriver_java/src/test/java/base/BaseTests.java
@@ -1,85 +1,10 @@
-//package base;
-//
-//import com.google.common.io.Files;
-//import org.openqa.selenium.OutputType;
-//import org.openqa.selenium.TakesScreenshot;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
-//import org.openqa.selenium.support.events.EventFiringWebDriver;
-//import org.testng.ITestResult;
-//import org.testng.annotations.AfterClass;
-//import org.testng.annotations.AfterMethod;
-//import org.testng.annotations.BeforeClass;
-//import org.testng.annotations.BeforeMethod;
-//import pages.Home;
-//import utils.EventReporter;
-//import utils.WindowManager;
-//
-//import java.io.File;
-//import java.io.IOException;
-//
-//public class BaseTests {
-//
-//    private EventFiringWebDriver driver;
-//    protected Home homePage;
-//
-//    @BeforeClass
-//    public void setUp(){
-//        System.setProperty("webdriver.chrome.driver", "/Users/lilitkarapetyan/Desktop/random/chromedriver");
-//        driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
-//        driver.register(new EventReporter());
-//        goHome();
-//        //setCookie();
-//    }
-//
-//    @BeforeMethod
-//    public void goHome(){
-//        driver.get("https://www.list.am/");
-//        homePage = new Home(driver);
-//    }
-//
-//    @AfterClass
-//    public void tearDown(){
-//        driver.quit();
-//    }
-//
-//    @AfterMethod
-//    public void recordFailure(ITestResult result){
-//        if(ITestResult.FAILURE == result.getStatus())
-//        {
-//            var camera = (TakesScreenshot)driver;
-//            File screenshot = camera.getScreenshotAs(OutputType.FILE);
-//            try{
-//                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
-//            }catch(IOException e){
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//
-//    public WindowManager getWindowManager(){
-//        return new WindowManager(driver);
-//    }
-//
-//    private ChromeOptions getChromeOptions(){
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("disable-infobars");
-//        //options.setHeadless(true);
-//        return options;
-//    }
-//
-//}
-
 package base;
 
 import com.google.common.io.Files;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
@@ -92,17 +17,17 @@ import utils.WindowManager;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class BaseTests {
 
-    protected WebDriver driver;
+    private EventFiringWebDriver driver;
     protected Home homePage;
 
     @BeforeClass
-    public void setUp() throws MalformedURLException {
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "/Users/lilitkarapetyan/Desktop/random/chromedriver");
+        driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
+        driver.register(new EventReporter());
         goHome();
     }
 
@@ -143,3 +68,77 @@ public class BaseTests {
     }
 
 }
+//
+//package base;
+//
+//import com.google.common.io.Files;
+//import org.openqa.selenium.OutputType;
+//import org.openqa.selenium.TakesScreenshot;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+//import org.openqa.selenium.support.events.EventFiringWebDriver;
+//import org.testng.ITestResult;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.BeforeMethod;
+//import pages.Home;
+//import utils.EventReporter;
+//import utils.WindowManager;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.net.MalformedURLException;
+//import java.net.URL;
+//
+//public class BaseTests {
+//
+//    protected WebDriver driver;
+//    protected Home homePage;
+//
+//    @BeforeClass
+//    public void setUp() throws MalformedURLException {
+//        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
+//        goHome();
+//    }
+//
+//    @BeforeMethod
+//    public void goHome(){
+//        driver.get("https://www.list.am/");
+//        homePage = new Home(driver);
+//    }
+//
+//    @AfterClass
+//    public void tearDown(){
+//        driver.quit();
+//    }
+//
+//    @AfterMethod
+//    public void recordFailure(ITestResult result){
+//        if(ITestResult.FAILURE == result.getStatus())
+//        {
+//            var camera = (TakesScreenshot)driver;
+//            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+//            try{
+//                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
+//            }catch(IOException e){
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    public WindowManager getWindowManager(){
+//        return new WindowManager(driver);
+//    }
+//
+//    private ChromeOptions getChromeOptions(){
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("disable-infobars");
+//        //options.setHeadless(true);
+//        return options;
+//    }
+//
+//}
